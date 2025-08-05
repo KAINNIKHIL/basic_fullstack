3d15584b7b8da7bfd5cb607c68c3d8306834c0f7
Nikhil <nikhilsinghkaintura@gmail.com>
2025-08-05 22:15:57 +0530

    models are ready now

--- server/index.js
+++ server/index.js
@@ -1,13 +1,24 @@
 import express from 'express';
+import mongoose  from 'mongoose';
+import { DB_NAME } from './constant.js';
+import dotenv from "dotenv";
+dotenv.config();
+
 
 const app = express();
-
-app.get('/',(req, res)=>{
-    res.send('Server is ready');
-});
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, ()=>{
-    console.log(`Serve at http://localhost:${port}`);
-});
+(async ()=> {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("Error:",error);
+            throw error;
+        })
+        app.listen(process.env.PORT ,()=>{
+            console.log(` Server running on port ${process.env.PORT}`);
+        })
+    }
+    catch(error){
+        console.log("Error:",error);
+        throw error;
+    }
+})();
---
+++ server/models/user.model.js
@@ -0,0 +1,71 @@
+import mongoose,{Schema} from "mongoose";
+import jwt from "jsonwebkton";
+import bcrypt from "bcrypt";
+
+export const User = mongoose.model("User", userSchema);
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true
+    },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+    },
+        phone: {
+            type: String,
+            required: true
+    },
+        password: {
+            type: String,
+            required: [true, 'Password is required']
+    },
+         refreshToken: {
+            type: String
+    }},
+    {
+        timestamps : true
+    }
+);
+    
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password")) return next();
+
+    this.password = bcrypt.hash(this.password,10)
+    next();
+})
+
+userSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            username: this.username,
+            email: this.email
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiryIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiryIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
